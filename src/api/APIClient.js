d8d2c11a4711a1f4875e83e0848af54f4ed4cbaf
Corentin Terente <44975843+CorentinTerente@users.noreply.github.com>
2019-01-15 16:05:30 +0100

    Api client (#1)
    * creating APIClient.js and adding loginUser and createUser functions
    
    * adding the rest of the api
    
    * refactored APIClient.js and removing a console log in ConnectContainer
    
    * adding updateUser function and modified package.json (npm run lin and lintfix

---
+++ src/api/APIClient.js
@@ -0,0 +1,157 @@
+export default class APIClient {
+  static loginUser(userPhoneNumber, userPassword) {
+    return fetch('https://familink-api.cleverapps.io/public/login',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          phone: userPhoneNumber,
+          password: userPassword,
+        }),
+      })
+      .then(response => response.json())
+      .then(responseJSON => responseJSON.token)
+      .catch();
+  }
+
+  static createUser(userPhoneNumber, userPassword, userFirstName,
+    userLastName, userEmail, userProfile) {
+    return fetch('https://familink-api.cleverapps.io/public/sign-in?contactsLength=0',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          phone: userPhoneNumber,
+          password: userPassword,
+          firstName: userFirstName,
+          lastName: userLastName,
+          email: userEmail,
+          profile: userProfile,
+        }),
+      })
+      .then(response => response.json())
+      .then(responseJSON => responseJSON)
+      .catch();
+  }
+
+  static forgotPassword(userPhoneNumber) {
+    return fetch('https://familink-api.cleverapps.io/public/forgot-password',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          phone: userPhoneNumber,
+        }),
+      })
+      .then(response => response.json())
+      .then(responseJSON => responseJSON)
+      .catch();
+  }
+
+  static updateUser(token, userFirstName, userLastName, userEmail, userProfile) {
+    return fetch('https://familink-api.cleverapps.io/secured/users',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        method: 'PUT',
+        body: JSON.stringify({
+          firstName: userFirstName,
+          lastName: userLastName,
+          email: userEmail,
+          profile: userProfile,
+        }),
+      })
+      .then(response => response.json())
+      .then(responseJSON => responseJSON)
+      .catch();
+  }
+
+  static getProfiles() {
+    return fetch('https://familink-api.cleverapps.io/public/profiles',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'GET',
+      })
+      .then(response => response.json())
+      .then(responseJSON => responseJSON)
+      .catch();
+  }
+
+  static getContacts(token) {
+    return fetch('https://familink-api.cleverapps.io/secured/users/contacts',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        method: 'GET',
+      })
+      .then(response => response.json())
+      .then(responseJSON => responseJSON)
+      .catch();
+  }
+
+  static deleteContact(token, idContact) {
+    return fetch(`https://familink-api.cleverapps.io/secured/users/contacts/${idContact}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        method: 'DELETE',
+      })
+      .then(response => response.json())
+      .then(responseJSON => responseJSON)
+      .catch();
+  }
+
+  static updateContact(token, idContact) {
+    fetch(`https://familink-api.cleverapps.io/secured/users/contacts/${idContact}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        method: 'PUT',
+      })
+      .then(response => response.json())
+      .then(responseJSON => responseJSON)
+      .catch();
+  }
+
+  static addContact(token, contactPhoneNumber, contactFirstName,
+    contactLastName, contactEmail, contactProfile, contactGravatar,
+    contactIsFamilinkUser, contactIsEmergencyUser) {
+    return fetch('https://familink-api.cleverapps.io/secured/users/contacts/',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        method: 'POST',
+        body: JSON.stringify({
+          phone: contactPhoneNumber,
+          firstName: contactFirstName,
+          lastName: contactLastName,
+          email: contactEmail,
+          profile: contactProfile,
+          gravatar: contactGravatar,
+          isFamilinkUser: contactIsFamilinkUser,
+          isEmergencyUser: contactIsEmergencyUser,
+        }),
+      })
+      .then(response => response.json())
+      .then(responseJSON => responseJSON)
+      .catch();
+  }
+}
--- src/component/ConnectContainer.js
+++ src/component/ConnectContainer.js
@@ -1,43 +1,41 @@
-import React, { Component } from "react";
-import { AppContainer } from '../screen/StackNavigator'
+import React, { Component } from 'react';
 import { NetInfo, View } from 'react-native';
+import { AppContainer } from '../screen/StackNavigator';
 
 
 export default class ConnectContainer extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            connected: undefined,
-        }
-        this.handleConnectivityChange = this.handleConnectivityChange.bind(this);
-    }
+    this.state = {
+      connected: undefined,
+    };
+    this.handleConnectivityChange = this.handleConnectivityChange.bind(this);
+  }
 
-    componentDidMount() {
-        NetInfo.isConnected.fetch().then((isConnected) => {
-            this.setState({ connected: isConnected })
-        })
-            .catch((error) => {
-                console.error(error);
-            })
-        NetInfo.isConnected.addEventListener(
-            'connectionChange',
-            this.handleConnectivityChange
-        );
-    }
+  componentDidMount() {
+    NetInfo.isConnected.fetch().then((isConnected) => {
+      this.setState({ connected: isConnected });
+    })
+      .catch();
+    NetInfo.isConnected.addEventListener(
+      'connectionChange',
+      this.handleConnectivityChange,
+    );
+  }
 
-    componentWillUnmount() {
-        NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
-    }
+  componentWillUnmount() {
+    NetInfo.isConnected.removeEventListener('connectionChange', this.handleConnectivityChange);
+  }
 
 
-    handleConnectivityChange(isConnected) {
-        isConnected ? this.setState({ isConnected: true }) : this.setState({ isConnected: false })
-    }
+  handleConnectivityChange(isConnected) {
+    isConnected ? this.setState({ isConnected: true }) : this.setState({ isConnected: false });
+  }
 
-    render() {
-        return (
-            <AppContainer />
-        )
-    }
+  render() {
+    return (
+      <AppContainer />
+    );
+  }
 }
