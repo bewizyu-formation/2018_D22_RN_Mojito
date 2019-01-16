09e6012c55babea5c8243a291606335ca9e45922
mathou <seigle.mathis@gmail.com>
2019-01-16 17:07:22 +0100

    fix import

--- src/screen/DetailScreen.js
+++ src/screen/DetailScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, Image, StyleSheet, ScrollView } from 'react-native';
+import { Text, View, TouchableOpacity, Image, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
