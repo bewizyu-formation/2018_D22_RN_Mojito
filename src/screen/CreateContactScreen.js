33105cfaf940bff37c9442a522347e7f093b862f
Etienne <etienne.graff@hotmail.fr>
2019-01-16 16:54:42 +0100

    add switch+check inputs

--- src/screen/CreateContactScreen.js
+++ src/screen/CreateContactScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { TextInput, Text, View, KeyboardAvoidingView, TouchableHighlight, StyleSheet, Image, Picker, Switch } from 'react-native';
+import { Platform, Alert, TextInput, Text, View, KeyboardAvoidingView, TouchableHighlight, StyleSheet, Image, Picker, Switch } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 
@@ -21,8 +21,7 @@ const styles = StyleSheet.create({
       borderColor: 'gray',
       borderWidth: 1,
       backgroundColor: '#FFFFFF',
-      marginTop: 5,
-      marginBottom: 15
+      marginTop: 5
     },
     logo: {
       width: 150,
@@ -31,7 +30,11 @@ const styles = StyleSheet.create({
       marginBottom: 10,
     },
     text: {
-      fontSize: 20
+      fontSize: 20,
+      marginTop:15
+    },
+    textAlert:{
+        color: "red"
     },
     textPrimaryButton: {
       fontWeight: 'bold',
@@ -80,9 +83,13 @@ export default class CreateContactScreen extends Component {
 
     this.state={
         lastname: '',
+        lastnameEmpty: false,
         firstname: '',
+        firstnameEmpty: false,
         phone: '',
+        invalidPhone: false,
         email: '',
+        invalidEmail: false,
         profil: 'famille',
         emergency: false
     }
@@ -102,27 +109,87 @@ export default class CreateContactScreen extends Component {
           style={styles.textInput}
           placeholder='Nom du contact'
           onChangeText={(value) => this.setState({lastname: value})}
+          onBlur={() => {
+            if(this.state.lastname.length === 0){
+                this.setState({lastnameEmpty: true});
+            }else {
+                this.setState({lastnameEmpty: false});
+            }
+        }}
         />
+        {this.state.lastnameEmpty? (
+            <Text style={styles.textAlert}>Le nom du contact doit être renseigné</Text>
+        ):(
+            null
+        )
+        }
+
         <Text style={styles.text}>Prénom</Text>
         <TextInput
           style={styles.textInput}
           placeholder='Prénom du contact'
           onChangeText={(value) => this.setState({firstname: value})}
+          onBlur={() => {
+            if(this.state.firstname.length === 0){
+                this.setState({firstnameEmpty: true});
+            }else {
+                this.setState({firstnameEmpty: false});
+            }
+        }}
         />
+        {this.state.firstnameEmpty? (
+            <Text style={styles.textAlert}>Le prénom du contact doit être renseigné</Text>
+        ):(
+            null
+        )
+        }
+
+
         <Text style={styles.text}>Numéro de téléphone</Text>
         <TextInput
           style={styles.textInput}
           placeholder='Numéro de téléphone du contact'
           keyboardType='phone-pad'
           onChangeText={(value) => this.setState({phone: value})}
+        onBlur={() => {
+            if(this.state.phone.length !== 10){
+                this.setState({invalidPhone: true});
+            }else {
+                this.setState({invalidPhone: false});
+            }
+        }}
         />
+        {this.state.invalidPhone? (
+            <Text style={styles.textAlert}>Numéro de téléphone invalide</Text>
+        ):(
+            null
+        )
+        }
+
         <Text style={styles.text}>Email</Text>
         <TextInput
           style={styles.textInput}
           placeholder='Adresse mail du contact'
           keyboardType='email-address'
           onChangeText={(value) => this.setState({email: value})}
+          onBlur={() => {
+            let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+            if(reg.test(this.state.email) === false)
+            {
+                this.setState({invalidEmail: true});
+            }
+            else {
+                this.setState({invalidEmail: false});
+            }
+        }}
         />
+        {this.state.invalidEmail? (
+            <Text style={styles.textAlert}>Le format de l'adresse mail n'est pas valide</Text>
+        ):(
+            null
+        )
+        }
+        
         
         <Text style={styles.text}>Avatar</Text>
         <TextInput
@@ -131,7 +198,8 @@ export default class CreateContactScreen extends Component {
         />
 
         <Text style={styles.text}>Profil</Text>
-        <Picker
+        {Platform.OS === "android" ? (
+            <Picker
             selectedValue={this.state.profil}
             prompt='Profil du contact'
             mode='dropdown'
@@ -141,13 +209,17 @@ export default class CreateContactScreen extends Component {
             <Picker.Item label='Senior' color='#FF6C00' value='senior' />
             <Picker.Item label='Médecin' color='#FF6C00' value='médecin' />
         </Picker>
+        ):(
+            null
+        )}
+        
 
         <View style={{flexDirection:'row'}}>
             <Text style={styles.text}>Contacter en cas d'urgence </Text>
             <Switch 
-                onValueChange={ (value) => this.setState({ emergency: value })} 
+                onValueChange={ (value) => this.setState({ emergency: value })}
+                style={{marginTop:15, marginLeft:10, transform: Platform.OS=== 'android'?[{ scaleX: 1.5 }, { scaleY: 1.5 }]:[{ scaleX: 1 }, { scaleY: 1 }] }} 
                 value={ this.state.emergency }
-                trackColor='#FF6C00' 
             /> 
 
         </View>
@@ -155,8 +227,16 @@ export default class CreateContactScreen extends Component {
         <View style={styles.buttonContainer}>
           <TouchableHighlight
             onPress={() => {
+
+                if(this.state.lastname !== '' && this.state.firstname !== '' 
+                    && this.state.phone !== '' && !this.state.invalidPhone 
+                    && this.state.email !== '' && !this.state.invalidEmail){
+                    this.props.navigation.navigate('Contacts');
+                } else {
+                    Alert.alert("Création impossible","Un ou plusieurs champs sont mal renseignés");
+                }
               
-                this.props.navigation.navigate('Contacts');
+                
               
             }}
             style={styles.primaryButton}
