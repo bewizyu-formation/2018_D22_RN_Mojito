9d75fc6763a6cb070e95355fb48acdb7a378b542
Corentin <corentin.terente@gmail.com>
2019-01-18 10:09:35 +0100

    added menu button in ContactsScreen.js and added MenuScreen.js

---
+++ src/component/AddButton.js
@@ -0,0 +1,45 @@
+import React,{Component} from 'react';
+import {
+  TouchableOpacity, Text,
+} from 'react-native';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+class AddButton extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <TouchableOpacity onPress={() => {
+                if(this.props.connectivity){
+                    this.props.navigation.navigate('CreateAccount');
+                } else {
+                    alert("Pas de connexion internet")
+                }
+            }}>
+                <Text>ADD</Text>
+            </TouchableOpacity>
+        )
+    }
+  
+}
+  
+  AddButton.propTypes = {
+    navigation: PropTypes.shape({
+        navigate: PropTypes.func.isRequired,
+    }).isRequired,
+    connectivity: PropTypes.bool.isRequired,
+  };
+  
+  const mapStateToProps = state => ({
+    connectivity: state.connect.connectivity,
+    
+  });
+  const mapDispatchToProps = dispatch => ({
+   
+  });
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(AddButton);
+  
